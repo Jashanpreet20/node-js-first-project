c9b85aecc579e195bd53e716514cc0e11625e12c
jashanpreet20 <jashankot20>
2023-02-19 14:04:39 +0530

    sending request and return custom response

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const http=require('http');
-
-const server=http.createServer(function(req,res) {
-    console.log(req);
-})
-
-server.listen(4000,() =>{
-    console.log('hello world')
+const server=http.createServer((req,res) =>{
+    const url=req.url;
+    console.log(url);
+   if(req.url === '/home')
+   {
+    res.end('welcome home');
+   }
+   else if(req.url === '/about')
+   {
+    res.end('welcome to about us page');
+   }else if(req.url === '/node')
+   {
+    res.end('welcome to my node js project');
+   }
 });
+
+
+server.listen(3000, () =>{
+    console.log('server run at 3000 port');
+})
